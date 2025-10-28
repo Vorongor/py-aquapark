e1701e8caf631d2370a3ffc2ca90ffea72b866fb
Vorongor <mr.maddarknes@gmail.com>
2025-10-28 09:55:25 +0200

    Remove unnecessary inits methods and correct mistake with params

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from abc import ABC
 class IntegerRange:
     def __init__(self,
                  min_amount: int,
-                 max_amount: int
-                 ) -> None:
+                 max_amount: int) -> None:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
@@ -16,14 +15,12 @@ class IntegerRange:
 
     def __get__(self,
                 instance: "IntegerRange",
-                owner: "IntegerRange"
-                ) -> int:
+                owner: "IntegerRange") -> int:
         return getattr(instance, self.protected_name)
 
     def __set__(self,
                 instance: "IntegerRange",
-                value: int
-                ) -> None:
+                value: int) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
         if not self.min_amount <= value <= self.max_amount:
@@ -38,8 +35,7 @@ class Visitor:
                  name: str,
                  age: int,
                  weight: int,
-                 height: int
-                 ) -> None:
+                 height: int) -> None:
         self.name = name
         self.age = age
         self.weight = weight
@@ -50,8 +46,7 @@ class SlideLimitationValidator(ABC):
     def __init__(self,
                  age: int,
                  weight: int,
-                 height: int
-                 ) -> None:
+                 height: int) -> None:
         self.age = age
         self.weight = weight
         self.height = height
@@ -62,32 +57,17 @@ class ChildrenSlideLimitationValidator(SlideLimitationValidator):
     height = IntegerRange(80, 120)
     weight = IntegerRange(20, 50)
 
-    def __init__(self,
-                 age: int,
-                 weight: int,
-                 height: int
-                 ) -> None:
-        super().__init__(age, weight, height)
-
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
     age = IntegerRange(14, 60)
     height = IntegerRange(120, 220)
     weight = IntegerRange(50, 120)
 
-    def __init__(self,
-                 age: int,
-                 weight: int,
-                 height: int
-                 ) -> None:
-        super().__init__(age, weight, height)
-
 
 class Slide:
     def __init__(self,
                  name: str,
-                 limitation_class: type[SlideLimitationValidator]
-                 ) -> None:
+                 limitation_class: type[SlideLimitationValidator]) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
